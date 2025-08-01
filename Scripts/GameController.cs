7b9f527c2324f79740d614e60f9b093a5cfc84eb
michiru24s1101225-png <michiru_24s1101225@nnn.ed.jp>
2025-08-01 17:35:31 +0900

    Initial commit

---
+++ Scripts/GameController.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+enum すごろく
+{
+    Lobby,
+    Start,
+    Move,
+    Stop,
+    NormalPoint,
+    PlusPoint,
+    MinusPoint,
+    Goal
+}
+
+enum PIPer
+{
+    PI2,//90*
+    PI,//180*
+    PIMinus2,//-90*
+    PIMinus1//-180*
+}
+public class GameController : MonoBehaviour
+{
+    float nowAngle = 0;
+    float timer = 0;
+    bool finished = false;
+    bool minus = true;
+    bool isPushed = false;
+    int PICount = 0;
+    int nowPoint = 0;
+    int saikoro;
+    すごろく GameWave;
+    PIPer pIPer;
+    Vector3 nowPos;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject player;
+    [SerializeField] List<PointDatas> pointDatas = new List<PointDatas>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        GameObject points = GameObject.Find("Points");
+        while (points.transform.childCount != i)
+        {
+            pointDatas.Add(points.transform.GetChild(i).GetComponent<PointDatas>());
+            i++;
+        }
+        GameWave = すごろく.Lobby;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (GameWave)
+        {
+            default:
+                GameWave = すごろく.NormalPoint;
+                break;
+            case すごろく.Lobby:
+                if (Input.GetAxis("Jump") > 0)
+                {
+                    GameWave++;
+                    text.text = "Start!!!";
+                }
+                break;
+            case すごろく.Start:
+                if (isPushed) return;
+                if (Input.GetKeyUp(KeyCode.Space)) isPushed = true;
+                if (isPushed)
+                {
+                    saikoro = UnityEngine.Random.Range(0, 6) + 1;
+                    text.text += "\nサイコロの目は" + (saikoro);
+                    if (nowPoint == pointDatas.Count)
+                    {
+                        GameWave = すごろく.Goal;
+                    }
+                    else
+                    {
+                        int i = 1;
+                        if (pointDatas[nowPoint].totalAngle < 0)
+                        {
+                            PICount = -1;
+                            minus = true;
+                            while (i < 5 && !(pointDatas[nowPoint].totalAngle <= 90 * -i))
+                            {
+                                PICount--;
+                                i++;
+                            }
+                        }
+                        else
+                        {
+                            PICount = 1;
+                            minus = false;
+                            while (i < 5 && !(pointDatas[nowPoint].totalAngle >= 90 * i))
+                            {
+                                PICount++;
+                                i++;
+                            }
+                        }
+                        GameWave = すごろく.Move;
+                        nowPos = player.transform.position;
+                        text.text += "\n移動中...";
+                    }
+                }
+                break;
+            case すごろく.Move:
+                float totalTime = 1;
+                float x1 = player.transform.position.x;
+                float z1 = player.transform.position.z;
+                float x2 = pointDatas[nowPoint + 1].transform.position.x;
+                float z2 = pointDatas[nowPoint + 1].transform.position.z;
+                if ((Mathf.Sqrt((x2 - x1) * (x2 - x1) + (z2 - z1) * (z2 - z1)) > 0.01f && !finished) && saikoro != 0)
+                {
+                    if (pointDatas[nowPoint].totalAngle == 0)
+                    {
+                        player.transform.position += ((pointDatas[nowPoint + 1].transform.position - nowPos) / totalTime) * Time.deltaTime;
+                    }
+                    else
+                    {
+                        float speed = 2;
+                        Vector3 pointA = pointDatas[nowPoint].transform.position;
+                        Vector3 pointB = pointDatas[nowPoint + 1].transform.position;
+                        Vector3 mypos = player.transform.position;
+                        Vector3 center = new Vector3(pointB.x, pointA.y, pointA.z);
+                        float l = Vector3.Distance(pointA, center);
+                        float m = Vector3.Distance(pointB, center);
+                        nowAngle = 0;
+                        if (minus)
+                        {
+                            nowAngle = (float)(Math.PI / -2) + timer * speed * -1;
+                            if (nowAngle <= (float)(Math.PI / -2) * PICount && nowAngle >= (float)(Math.PI / -2) * 3)
+                            {
+                                mypos.x = l * Mathf.Sin(nowAngle) + center.x;
+                                mypos.z = m * Mathf.Cos(nowAngle) + center.z;
+                            }
+                            else
+                            {
+                                finished = true;
+                            }
+                        }
+                        else
+                        {
+                            nowAngle = (float)(Math.PI / 2) + timer * speed * 1;
+                            if (nowAngle <= (float)(Math.PI / 2) * PICount && nowAngle >= (float)(Math.PI / 2) * 3)
+                            {
+                                mypos.x = l * Mathf.Sin(nowAngle) + center.x;
+                                mypos.z = m * Mathf.Cos(nowAngle) + center.z;
+                            }
+                            else
+                            {
+                                finished = true;
+                            }
+                        }
+                        player.transform.position = mypos;
+                        timer += Time.deltaTime;
+                    }
+                }
+                else if (saikoro != 0)
+                {
+                    finished = false;
+                    PICount = 0;
+                    timer = 0;
+                    nowPos = player.transform.position;
+                    int i = 1;
+                    if (pointDatas[nowPoint].totalAngle < 0)
+                    {
+                        PICount = -1;
+                        minus = true;
+                        while (i < 5 && !(pointDatas[nowPoint].totalAngle <= 90 * -i))
+                        {
+                            PICount--;
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        PICount = 1;
+                        minus = false;
+                        while (i < 5 && !(pointDatas[nowPoint].totalAngle >= 90 * i))
+                        {
+                            PICount++;
+                            i++;
+                        }
+                    }
+                    if (nowPoint < 0)
+                    {
+                        if (minus)
+                        {
+                            minus = false;
+                        }
+                        else
+                        {
+                            minus = true;
+                        }
+                    }
+                    saikoro--;
+                    nowPoint++;
+                    if (nowPoint + 1 == pointDatas.Count) GameWave = すごろく.Goal;
+                }
+                else
+                {
+                    timer = 0;
+                    isPushed = false;
+                    GameWave++;
+                }
+                break;
+            case すごろく.Stop:
+                GameWave += pointDatas[nowPoint].pointEffect;
+                text.text += "\n何かのマスにとまった!!!";
+                break;
+            case すごろく.NormalPoint:
+                GameWave = すごろく.Start;
+                text.text += "何もなかった。";
+                break;
+            case すごろく.PlusPoint:
+                saikoro++;
+                GameWave = すごろく.Move;
+                text.text += "\nプラスマス。1歩前進!!!";
+                break;
+            case すごろく.MinusPoint:
+                saikoro++;
+                nowPoint -= 2;
+                GameWave = すごろく.Move;
+                text.text += "\nマイナスマス。1歩後退...\n移動中...";
+                break;
+            case すごろく.Goal:
+                text.text = "GOAL!!!";
+                break;
+        }
+    }
+
+}
---
+++ Scripts/PointDatas.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointDatas : MonoBehaviour
+{
+    public Vector3 pointTransform;
+    public int totalAngle = 0;
+    public int pointEffect;
+}
